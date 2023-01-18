5873f2689a39eebdb967f31cfe9b3281a5ff352a
vsevolod_konkin <sevakon@mail.ru>
2023-01-18 19:08:04 +0300

    dev

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.io.File;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String folderPath = "/Users/user/Desktop/vs";
+        File file = new File(folderPath);
+        System.out.println(getFolderSize(file));
+    }
+    public static long getFolderSize(File folder) {
+        if (folder.isFile()) {
+            return folder.length();
+        }
+        File[] files = folder.listFiles();
+        long sum = 0;
+        for (File file : files) {
+            sum += getFolderSize(file);
+        }
+        return sum;
     }
 }
